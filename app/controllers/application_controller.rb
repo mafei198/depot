2f1b315c45d7a16a9ba3bffb2bc6c60a0c385282
mafei.198 <mafei.198@gmail.com>
2011-05-03 22:01:23 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+    def count_come_in
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter] += 1
+      end
+    end
+    def reset_count
+      session[:counter] = 0
+    end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @times = count_come_in
     @products = Product.all
   end
 
